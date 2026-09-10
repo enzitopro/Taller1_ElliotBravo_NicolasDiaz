8af68336e20434ce7a55b05581a36dd0171a3b62
enzitopro <ajipicantemrdayt@gmail.com>
2026-09-10 10:41:26 -0300

    crear menu hospital

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    cout << "creamos el repo del taller" << endl;
-    cout << "hola mundo" << endl;
+
+    int opcion;
+    do {
+        cout << "=== HOSPITAL MARMAJA ===" << endl;
+        cout << "A. Atender pacientes" << endl;
+        cout << "B. Ver departamento" << endl;
+        cout << "C. Revisar historial de atencion" << endl;
+        cout << "D. Salir" << endl;
+        cout << "Seleccionar opcion: ";
+
+        cin << opcion;
+    } while (opcion != 4);
+    cout << "Hasta luego :D" << endl;
+
+    return 0;
 }
