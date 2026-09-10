7de7d7ac637ac0f93d121d3c3ad11ea0fc956bb0
enzitopro <ajipicantemrdayt@gmail.com>
2026-09-10 11:59:37 -0300

    crear clase source de personas y pacientes

---
+++ Modelos.cpp
@@ -0,0 +1,23 @@
+#include "Modelos.hpp"
+#include <iostream>
+
+using namespace std;
+
+Persona::Persona(string n, int e) {
+    nombre = n;
+    edad = e;
+}
+
+string Persona::getNombre() {
+    return nombre;
+}
+int Persona::getEdad() {
+    return edad;
+}
+
+Paciente::Paciente(string i, string n, int e, string s) : Persona(n, e) {
+    id = i;
+    servicioDestino = s;
+}
+
+
