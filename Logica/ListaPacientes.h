e792c991af777b965fc30fa2b405b1766dc68b88
Nicolas Diaz <nicolas.diaz04@alumnos.ucn.cl>
2026-09-11 18:58:11 -0300

    Creación de clases -Se agrego ListaPaciente.h

---
+++ Logica/ListaPacientes.h
@@ -0,0 +1,22 @@
+#ifndef TALLER1_ELLIOTBRAVO_NICOLASDIAZ_LISTAPACIENTES_H
+#define TALLER1_ELLIOTBRAVO_NICOLASDIAZ_LISTAPACIENTES_H
+
+#endif //TALLER1_ELLIOTBRAVO_NICOLASDIAZ_LISTAPACIENTES_H
+
+#include "NodoPaciente.h"
+
+class ListaPacientes {
+ private:
+   NodoPaciente* inicio;
+
+ public:
+   ListaPacientes();
+
+   ~ListaPacientes();
+    //metodos
+   void agregarPaciente(Paciente* p);
+   void mostrarPacientes() const;
+
+};
+
+#endif;
